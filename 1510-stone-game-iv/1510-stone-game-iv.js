26354fa324971dc7640983eda3039fc324f8e459
HARSH TRIVEDI <harshtrivedi4480@gmail.com>
2026-08-10 22:56:32 +0530

    Time: 17 ms (87.5%), Space: 56.5 MB (75%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.js
@@ -0,0 +1,14 @@
+var winnerSquareGame = function(n) {
+    const dp = new Array(n + 1).fill(false);
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j * j <= i; j++) {
+            if (dp[i - j * j] === false) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[n];
+};
