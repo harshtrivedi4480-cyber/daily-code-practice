ffe2a0311f4991e00e542cec03916d2f65687795
HARSH TRIVEDI <harshtrivedi4480@gmail.com>
2026-08-12 20:11:46 +0530

    Time: 80 ms (46.67%), Space: 99.2 MB (53.33%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.js
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.js
@@ -2,29 +2,42 @@ var numberOfSubmatrices = function(grid) {
     const m = grid.length;
     const n = grid[0].length;
 
-    const x = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
-    const y = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+    const xPrefix = Array.from(
+        { length: m + 1 },
+        () => new Array(n + 1).fill(0)
+    );
+
+    const yPrefix = Array.from(
+        { length: m + 1 },
+        () => new Array(n + 1).fill(0)
+    );
 
     let ans = 0;
 
     for (let i = 1; i <= m; i++) {
         for (let j = 1; j <= n; j++) {
 
-            x[i][j] =
-                x[i - 1][j] +
-                x[i][j - 1] -
-                x[i - 1][j - 1] +
-                (grid[i - 1][j - 1] === 'X' ? 1 : 0);
+            const isX = grid[i - 1][j - 1] === 'X' ? 1 : 0;
+            const isY = grid[i - 1][j - 1] === 'Y' ? 1 : 0;
 
-            y[i][j] =
-                y[i - 1][j] +
-                y[i][j - 1] -
-                y[i - 1][j - 1] +
-                (grid[i - 1][j - 1] === 'Y' ? 1 : 0);
+            xPrefix[i][j] =
+                isX +
+                xPrefix[i - 1][j] +
+                xPrefix[i][j - 1] -
+                xPrefix[i - 1][j - 1];
+
+            yPrefix[i][j] =
+                isY +
+                yPrefix[i - 1][j] +
+                yPrefix[i][j - 1] -
+                yPrefix[i - 1][j - 1];
 
             // Equal X and Y
-            // At least one X
-            if (x[i][j] === y[i][j] && x[i][j] > 0) {
+            // AND at least one X
+            if (
+                xPrefix[i][j] === yPrefix[i][j] &&
+                xPrefix[i][j] > 0
+            ) {
                 ans++;
             }
         }
