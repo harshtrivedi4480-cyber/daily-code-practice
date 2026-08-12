f2b7b32d74e826e27b2f1bf68d6a26a2b6abf6fa
HARSH TRIVEDI <harshtrivedi4480@gmail.com>
2026-08-12 20:13:45 +0530

    Time: 54 ms (81.63%), Space: 74.3 MB (73.47%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.js
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.js
@@ -1,19 +1,27 @@
 var maxSubarrayLength = function(nums, k) {
-    let freq = new Map();
+    const freq = new Map();
+
     let left = 0;
     let ans = 0;
 
     for (let right = 0; right < nums.length; right++) {
-        // Add current element
-        freq.set(nums[right], (freq.get(nums[right]) || 0) + 1);
+        // Add nums[right]
+        freq.set(
+            nums[right],
+            (freq.get(nums[right]) || 0) + 1
+        );
 
-        // If frequency exceeds k, shrink window
+        // Window invalid hai to shrink karo
         while (freq.get(nums[right]) > k) {
-            freq.set(nums[left], freq.get(nums[left]) - 1);
+            freq.set(
+                nums[left],
+                freq.get(nums[left]) - 1
+            );
+
             left++;
         }
 
-        // Current window is good
+        // Current window valid hai
         ans = Math.max(ans, right - left + 1);
     }
 
