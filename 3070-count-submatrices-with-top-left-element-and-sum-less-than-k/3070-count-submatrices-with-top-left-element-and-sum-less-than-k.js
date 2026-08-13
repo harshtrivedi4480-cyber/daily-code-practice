e5b1db0c16ea6480d84e434def8f4a7825c536a5
HARSH TRIVEDI <harshtrivedi4480@gmail.com>
2026-08-13 20:58:56 +0530

    Time: 19 ms (64.29%), Space: 82.3 MB (14.29%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.js
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.js
@@ -2,26 +2,23 @@ var countSubmatrices = function(grid, k) {
     const m = grid.length;
     const n = grid[0].length;
 
-    // prefix[i][j] = sum of rectangle
-    // from (0,0) to (i-1,j-1)
-    const prefix = Array.from(
-        { length: m + 1 },
-        () => new Array(n + 1).fill(0)
-    );
-
     let ans = 0;
+
+    // Prefix sum matrix
+    const prefix = Array.from({ length: m + 1 }, () =>
+        new Array(n + 1).fill(0)
+    );
 
     for (let i = 1; i <= m; i++) {
         for (let j = 1; j <= n; j++) {
 
             prefix[i][j] =
-                grid[i - 1][j - 1]
-                + prefix[i - 1][j]
-                + prefix[i][j - 1]
-                - prefix[i - 1][j - 1];
+                grid[i - 1][j - 1] +
+                prefix[i - 1][j] +
+                prefix[i][j - 1] -
+                prefix[i - 1][j - 1];
 
-            // This prefix sum represents the submatrix
-            // from (0,0) to (i-1,j-1)
+            // Submatrix from (0,0) to (i-1,j-1)
             if (prefix[i][j] <= k) {
                 ans++;
             }
