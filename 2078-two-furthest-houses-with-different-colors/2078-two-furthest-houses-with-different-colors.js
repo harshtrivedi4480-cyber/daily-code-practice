92ba95550ae815917a4ef6380dc6a72e421280a2
HARSH TRIVEDI <harshtrivedi4480@gmail.com>
2026-09-15 23:12:52 +0530

    Time: 0 ms (100%), Space: 53.6 MB (53.25%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.js
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.js
@@ -1,22 +1,20 @@
 var maxDistance = function(colors) {
-    let n = colors.length;
-    let maxDist = 0;
+    const n = colors.length;
+    let ans = 0;
 
-    // Find the farthest house from the first house
-    for (let j = n - 1; j >= 0; j--) {
-        if (colors[j] !== colors[0]) {
-            maxDist = Math.max(maxDist, j);
-            break;
+    // Compare the first house with every other house
+    for (let j = 1; j < n; j++) {
+        if (colors[0] !== colors[j]) {
+            ans = Math.max(ans, j);
         }
     }
 
-    // Find the farthest house from the last house
-    for (let i = 0; i < n; i++) {
+    // Compare the last house with every other house
+    for (let i = 0; i < n - 1; i++) {
         if (colors[i] !== colors[n - 1]) {
-            maxDist = Math.max(maxDist, n - 1 - i);
-            break;
+            ans = Math.max(ans, n - 1 - i);
         }
     }
 
-    return maxDist;
+    return ans;
 };
