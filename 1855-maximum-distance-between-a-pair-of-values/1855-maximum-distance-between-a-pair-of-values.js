9e348011e856cb5fcf7d6f359a751e63058f6616
HARSH TRIVEDI <harshtrivedi4480@gmail.com>
2026-09-15 23:11:55 +0530

    Time: 2 ms (55.17%), Space: 69.7 MB (48.28%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.js
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.js
@@ -4,15 +4,17 @@ var maxDistance = function(nums1, nums2) {
     let maxDist = 0;
 
     while (i < nums1.length && j < nums2.length) {
-        if (nums1[i] <= nums2[j]) {
+        if (i <= j && nums1[i] <= nums2[j]) {
             // Valid pair
             maxDist = Math.max(maxDist, j - i);
+
+            // Try to increase distance
             j++;
         } else {
-            // nums1[i] is too large
+            // Either i > j or nums1[i] > nums2[j]
             i++;
-            
-            // j must always be >= i
+
+            // Make sure j is never behind i
             if (j < i) {
                 j = i;
             }
